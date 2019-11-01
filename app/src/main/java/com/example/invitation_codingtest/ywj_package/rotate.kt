2c6962b99842cc7def48ffb5e5f242749f7b5ed6
yoonswonjoon <doojoons@naver,com>
2019-11-02 01:37:23 +0900

    level 1

--- app/src/main/java/com/example/invitation_codingtest/ywj_package/rotate.kt
+++ app/src/main/java/com/example/invitation_codingtest/ywj_package/rotate.kt
@@ -5,13 +5,7 @@ fun main(args: Array<String>) {
     fun solution(A: ArrayList<Int>, K: Int): IntArray{
         if ((K%A.size==0) or (A.size==0))
             return A.toIntArray()
-        else if (K%(A.size)>A.size-(K%A.size)){
-            val n=K%A.size
-            val arraya = A.slice(A.size-n until  A.size)
-            val arrayb = A.slice(0 until  A.size-n)
-            val result = arraya + arrayb
-            return result.toIntArray()
-        }
+
         else {
             val n=K%A.size
             val arraya = A.slice(0 until A.size-n)
