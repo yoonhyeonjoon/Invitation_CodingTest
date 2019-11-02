ba515c49f2e07ede557d2e4ec77640f0f0295578
yoonswonjoon <doojoons@naver,com>
2019-11-02 23:15:25 +0900

    Merge branch 'master' of C:\App\Invitation_CodingTest with conflicts.

--- app/src/main/java/tette/wjtest.kt.kt
+++ app/src/main/java/tette/wjtest.kt.kt
@@ -1,48 +1,24 @@
 import kotlin.collections.ArrayList
+import kotlin.math.absoluteValue
 
 fun main(args: Array<String>) {
 
-    fun solution(A: ArrayList<Int>, K: Int): IntArray{
-        if ((K%A.size==0) or (A.size==0))
-            return A.toIntArray()
-        else if (K%(A.size)>A.size-(K%A.size)){
-            val n=K%A.size
-            val arraya = A.slice(A.size-n until  A.size)
-            val arrayb = A.slice(0 until  A.size-n)
-            val result = arraya + arrayb
-            return result.toIntArray()
+    fun solution(A: IntArray): Int {
+        val max = A.sum()
+        var result = (max-2*A[0]).absoluteValue
+        for (i in 1 until  A.size-1){
+            var b=A.slice(0..i).sum()
+           if (result>(max-2*b).absoluteValue){
+               result = (max-2*b).absoluteValue
+           }
         }
-        else {
-            val n=K%A.size
-            val arraya = A.slice(0 until A.size-n)
-            val arrayb = A.slice(A.size-n until A.size)
-            val result = arrayb + arraya
-            return result.toIntArray()
-        }
+        return result
     }
 
- //                                         7 6 1 1 2 6 6 3 8 9
-    val A = arrayListOf<Int>(3,8,9,7,6,1,1,2,6,6)
-    val K = 13
-    println(solution(A,K).toList())
+
+
+
+    println(solution(arrayOf(3,1,2,4,3).toIntArray()))
 
 
 }
-/* fun solution(A: IntArray, K: Int): IntArray{
-        if ((K%A.size==0) or (A.size==0))
-            return A
-        else if (K%(A.size)>A.size-(K%A.size)){
-            val n=K%A.size
-            val arraya = A.slice(A.size-n until  A.size)
-            val arrayb = A.slice(0 until  A.size-n)
-            val result = arraya + arrayb
-            return result.toIntArray()
-        }
-        else {
-            val n=K%A.size
-            val arraya = A.slice(0 until A.size-n)
-            val arrayb = A.slice(A.size-n until A.size)
-            val result = arrayb + arraya
-            return result.toIntArray()
-        }
-    }*/
