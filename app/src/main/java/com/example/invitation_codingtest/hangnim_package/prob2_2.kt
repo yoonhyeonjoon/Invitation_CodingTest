5999be8751609599344ff7df5083682f62e3159b
윤현준 <incarnatara@gmail.com>
2019-11-06 00:21:20 +0900

    20191106

--- app/src/main/java/com/example/invitation_codingtest/hangnim_package/prob2_2.kt
+++ app/src/main/java/com/example/invitation_codingtest/hangnim_package/prob2_2.kt
@@ -1,21 +1,27 @@
+import android.service.autofill.Validators.and
+import java.util.*
 
 fun main(args : Array<String>){
 
 
     fun solution(A: IntArray): Int {
 
-        val andarr = Array<Int>(A.size){1}
-        val checkarr = Array<Int>(A.size){0}
-
-        var anum = 0.toBigInteger()
-        for(a in A)
+        var checkmax = 0
+        val bitsetcheck = BitSet(A.size)
+        var resultnum = 0
+        for(idx in A.indices)
         {
-            anum = (1 shl a-1).toBigInteger() or anum
+            checkmax = Math.max(checkmax, A[idx])
+            bitsetcheck.set(A[idx], true)
+            val getbit = bitsetcheck.get(0,checkmax)
+            if(bitsetcheck.cardinality() == checkmax)
+                resultnum++
         }
-        return 0
+        return resultnum
     }
 
-    var A = intArrayOf(1000, 300,9,6,1,3,4,2,5)
+    var A = intArrayOf(1,2,7,4,5,6,3)
+  //  var A = intArrayOf(1,5,3,4,2,15, 40,9,6)
     val B = solution(A)
     println(B)
 
